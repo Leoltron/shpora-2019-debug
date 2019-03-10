2892ae5ad4d079fc9ef5fa0cd7bfb662c9c53e0b
Leoltron <leoole5@gmail.com>
2019-03-10 18:31:16 +0500

    No LINQ

--- JPEG/Utilities/MathEx.cs
+++ JPEG/Utilities/MathEx.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Linq;
+using System;
+using System.Threading.Tasks;
 
 namespace JPEG.Utilities
 {
     public static class MathEx
     {
-        public static double Sum(int from, int to, Func<int, double> function)
-            => Enumerable.Range(from, to - from).Sum(function);
-
         public static double SumByTwoVariables(int from1, int to1, int from2, int to2, Func<int, int, double> function)
-            => Sum(from1, to1, x => Sum(from2, to2, y => function(x, y)));
+        {
+            var sum = 0d;
+            for (var i1 = from1; i1 < to1; i1++)
+            for (var i2 = from2; i2 < to2; i2++)
+                sum += function(i1, i2);
 
         public static double LoopByTwoVariables(int from1, int to1, int from2, int to2, Action<int, int> function)
             => Sum(from1, to1, x => Sum(from2, to2, y =>
