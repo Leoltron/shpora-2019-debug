0969089c8a74f5fde35e181f405d128f43bf9751
Leoltron <leoole5@gmail.com>
2019-03-10 15:25:34 +0500

    Converted PixelFormat into enum

--- JPEG/Images/Pixel.cs
+++ JPEG/Images/Pixel.cs
@@ -5,41 +5,44 @@ namespace JPEG.Images
 {
     public class Pixel
     {
-        private readonly PixelFormat format;
 
         public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
         {
-            if (!new[]{PixelFormat.RGB, PixelFormat.YCbCr}.Contains(pixelFormat))
-                throw new FormatException("Unknown pixel format: " + pixelFormat);
-            format = pixelFormat;
-            if (pixelFormat == PixelFormat.RGB)
+            switch (pixelFormat)
             {
-                r = firstComponent;
-                g = secondComponent;
-                b = thirdComponent;
-            }
-            if (pixelFormat == PixelFormat.YCbCr)
-            {
-                y = firstComponent;
-                cb = secondComponent;
-                cr = thirdComponent;
+                case PixelFormat.RGB:
+                    R = firstComponent;
+                    G = secondComponent;
+                    B = thirdComponent;
+
+                    Y = 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
+                    Cb = 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
+                    Cr = 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+                    break;
+                case PixelFormat.YCbCr:
+                    Y = firstComponent;
+                    Cb = secondComponent;
+                    Cr = thirdComponent;
+
+                    R = (298.082 * Y + 408.583 * Cr) / 256.0 - 222.921;
+                    G = (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
+                    B = (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+                    break;
+                default:
+                    throw new FormatException("Unknown pixel format: " + pixelFormat);
             }
         }
 
-        private readonly double r;
-        private readonly double g;
-        private readonly double b;
+        public double R { get; }
 
-        private readonly double y;
-        private readonly double cb;
-        private readonly double cr;
+        public double G { get; }
 
-        public double R => format == PixelFormat.RGB ? r : (298.082 * y + 408.583 * Cr) / 256.0 - 222.921;
-        public double G => format == PixelFormat.RGB ? g : (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-        public double B => format == PixelFormat.RGB ? b : (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+        public double B { get; }
 
-        public double Y => format == PixelFormat.YCbCr ? y : 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-        public double Cb => format == PixelFormat.YCbCr ? cb : 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-        public double Cr => format == PixelFormat.YCbCr ? cr : 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
+        public double Y { get; }
+
+        public double Cb { get; }
+
+        public double Cr { get; }
     }
 }
