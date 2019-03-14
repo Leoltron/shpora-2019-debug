11171571b1afce60d92eb22336b58e4c3da4a67e
Leoltron <leoole5@gmail.com>
2019-03-14 16:46:28 +0500

    Pixel now consists of 3 bytes, not 6 doubles (8 times lesser)

--- JPEG/Images/Pixel.cs
+++ JPEG/Images/Pixel.cs
@@ -3,10 +3,14 @@ using System.Linq;
 
 namespace JPEG.Images
 {
-    public class Pixel
+    public struct Pixel
     {
+        public Pixel(double a, double b, double c, PixelFormat format)
+            : this(ToByte(a), ToByte(b), ToByte(c), format)
+        {
+        }
 
-        public Pixel(double firstComponent, double secondComponent, double thirdComponent, PixelFormat pixelFormat)
+        public Pixel(byte firstComponent, byte secondComponent, byte thirdComponent, PixelFormat pixelFormat)
         {
             switch (pixelFormat)
             {
@@ -14,35 +18,40 @@ namespace JPEG.Images
                     R = firstComponent;
                     G = secondComponent;
                     B = thirdComponent;
-
-                    Y = 16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0;
-                    Cb = 128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0;
-                    Cr = 128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0;
                     break;
                 case PixelFormat.YCbCr:
-                    Y = firstComponent;
-                    Cb = secondComponent;
-                    Cr = thirdComponent;
+                    var Y = firstComponent;
+                    var Cb = secondComponent;
+                    var Cr = thirdComponent;
 
-                    R = (298.082 * Y + 408.583 * Cr) / 256.0 - 222.921;
-                    G = (298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576;
-                    B = (298.082 * Y + 516.412 * Cb) / 256.0 - 276.836;
+                    R = ToByte((298.082 * Y + 408.583 * Cr) / 256.0 - 222.921);
+                    G = ToByte((298.082 * Y - 100.291 * Cb - 208.120 * Cr) / 256.0 + 135.576);
+                    B = ToByte((298.082 * Y + 516.412 * Cb) / 256.0 - 276.836);
                     break;
                 default:
                     throw new FormatException("Unknown pixel format: " + pixelFormat);
             }
         }
 
-        public double R { get; }
+        public byte R { get; }
 
-        public double G { get; }
+        public byte G { get; }
 
-        public double B { get; }
+        public byte B { get; }
 
-        public double Y { get; }
+        public byte Y => ToByte(16.0 + (65.738 * R + 129.057 * G + 24.064 * B) / 256.0);
+        public byte Cb => ToByte(128.0 + (-37.945 * R - 74.494 * G + 112.439 * B) / 256.0);
+        public byte Cr => ToByte(128.0 + (112.439 * R - 94.154 * G - 18.285 * B) / 256.0);
 
-        public double Cb { get; }
 
-        public double Cr { get; }
+        public static byte ToByte(double d)
+        {
+            var val = (int) d;
+            if (val > byte.MaxValue)
+                return byte.MaxValue;
+            if (val < byte.MinValue)
+                return byte.MinValue;
+            return (byte) val;
+        }
     }
 }
